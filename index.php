4e966da2b726a1480223ac636338a396988be039
hxii <paul@glushak.net>
2020-05-06 21:38:48 +0300

    u.2 branch
    Iteration u.2 changes the functionality of the Micro theme by replacing the entry listing with a 1 by 1 entry display with just next and previous links (it's best to use position values to change the order of entries).

--- index.php
+++ index.php
@@ -17,39 +17,31 @@
 	<body>
 		<div class="container<?php echo ($page->template())?? ''; ?>">
 			<?php
-			if ( $WHERE_AM_I === 'home' ) { ?>
+			$pages->sortByPosition(true);
+			if ( $WHERE_AM_I === 'home' ) {
+				$posts = array_reverse($pages->getList(1,-1,1,0));
+				$page = new Page($posts[0]);
+			} ?>
 			<header class="mb2">
-				<?php echo $site->title(); ?>
-			</header>
 				<?php
-				$posts = $pages->getList(0,-1,1,0,1);
-				echo '<div class="entries mb2">';
-				foreach ( $posts as $page ) {
-					$page = new Page($page);
-					?>
-					<div class="entry mbh">
-						<a href="<?php echo $page->permalink(); ?>"><?php echo $page->title(); ?></a>
-						<span class="meta g">&mdash; <?php echo $page->date("d.m.y");?><?php echo ($page->category())? ', ' . $page->category() : ''; ?></span>
-					</div>
-					<?php
+				$next_page = new Page($page->nextKey());
+				$prev_page = new Page($page->previousKey());
+				if ( $prev_page->key() !== false ) {
+					echo "<a rel='prev' href='{$prev_page->permalink()}'>&larr;</a>";
+				} else {
+					echo "&times;";
 				}
-				echo '</div>';
-			}
-			if ( $WHERE_AM_I === 'page' ) {
-				?>
-				<header class="mb2">
-					<?php if ( $page->parent() ) {
-						$parent = new Page($page->parent());
-						$p = '<a href="'.$parent->permalink().'">'.$parent->title().'</a> / ';
-					} ?>
-					<a href="<?php echo $site->url(); ?>"><?php echo $site->title(); ?></a> / <?php echo ($p)?? ''; ?><?php echo $page->title(); ?></header>
-				<?php if ($page->coverImage()): ?>
-					<div class="cover"><img src="<?php echo $page->coverImage(); ?>" alt="<?php echo $site->title(); ?></a>"></div>
-				<?php endif; ?>
-				<div class="entry mb2">
-					<?php echo $page->content(); ?>
-				</div><?php } ?>
-			<div class="footer"><?php echo $site->footer(); ?></div>
+				echo ' / ';
+				if ( $next_page->key() !== false ) {
+					echo "<a rel='next' href='{$next_page->permalink()}'>&rarr;</a>";
+				} else {
+					echo "&times;";
+				}
+				?></header>
+			<div class="entry mb2"><?php echo $page->content(); ?></div>
+			<div class="footer">
+				<a rel="canonical" href="<?php echo $site->url(); ?>"><img src="<?php echo $site->logo(); ?>" style="max-height:3rem;width:auto;"><?php echo $site->footer(); ?></a>
+			</div>
 		</div>
 	</body>
 </html>
